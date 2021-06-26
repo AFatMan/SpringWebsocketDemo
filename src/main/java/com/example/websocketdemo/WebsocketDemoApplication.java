c623bccc2dcacbb756e557da3c9c85d55c56a2ca
Liujs <1453052796@qq.com>
2021-06-26 15:10:41 +0800

    官网示例

---
+++ src/main/java/com/example/websocketdemo/WebsocketDemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.websocketdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketDemoApplication {
+
+    /**
+     * localhost:8080
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketDemoApplication.class, args);
+    }
+
+}
